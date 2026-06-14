c38fa89da4683e8a23905c156c3e7f50b65f72e8
nieves geslin <geslinantoine0@gmail.com>
2026-06-14 15:19:20 +0200

    inventaire infusion

--- scripts/take_infusion.gd
+++ scripts/take_infusion.gd
@@ -1,15 +1,15 @@
 extends StaticBody3D
 
-@export var combination: Array[String]
-var player
+@export var id: int
 var souris_dessus := false
-# Called when the node enters the scene tree for the first time.
+var i := 0
+
+@onready var ui = get_node("/root/Test/UI")
+
 func _ready() -> void:
-	player = get_node("/root/Test/Player/ElementMagic")
-	print(player)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-
+	
 func _on_mouse_entered():
 	souris_dessus = true
 
@@ -19,5 +19,5 @@ func _on_mouse_exited():
 
 func _input(_event):
 	if Input.is_action_just_pressed("prendre_sort") && souris_dessus:
-		print(player.used_combination)
-		player.used_combination = combination
+		i += 1
+		ui.labels[id].text = str(i)
---
+++ scripts/ui.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+@onready var labels = [
+	$HBoxContainer/Eau/Label,
+	$HBoxContainer/Terre/Label,
+	$HBoxContainer/Feu/Label,
+	$HBoxContainer/Air/Label,
+]
