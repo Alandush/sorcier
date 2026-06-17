cefc22585ed1c7351b43f06aae698a7f4f33b75c
nieves geslin <geslinantoine0@gmail.com>
2026-06-17 12:42:05 +0200

    create spell condition minimum 2 infu

--- scripts/create_spell_button.gd
+++ scripts/create_spell_button.gd
@@ -2,7 +2,7 @@ extends PanelContainer
 
 var element_magic
 var create_spell_ui: VBoxContainer
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	element_magic = get_node("/root/Test/Player/ElementMagic")
 	create_spell_ui = get_node("/root/Test/UI/BoxCreateSpell")
@@ -10,6 +10,14 @@ func _ready() -> void:
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var child = create_spell_ui.get_child_count()
+		var id = create_spell_ui.box_spells.size()
+		var count = 0; # on vérifie combien de cases n'ont pas d'élément
+		for i in range(id):
+			if not create_spell_ui.box_spells[i].text:
+				count += count
+				if count - id < 2: # si il n'y a pas au moins 2 cases avec un élément
+					print('Pas assez de spell')
+					return;
 		for i in range(child):
 			element_magic.used_combination[i] = create_spell_ui.box_spells[i].text
 		print(element_magic.used_combination)
